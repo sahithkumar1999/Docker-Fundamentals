5a4fcb1b28a67c6ab5cd3b6dad81ed12a4efa70d
Sahith Kumar <sahithkumar1999@gmail.com>
2023-04-05 09:03:18 +0200

    strings practice

---
+++ Practice/Strings/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Sahith Kumar";
+            var lastName = "Singari";
+
+            var fullName = firstName + " " + lastName; // concatination
+
+            // concatination using string.format
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(", ", names);
+            Console.WriteLine(formattedNames);
+
+            var text = "Hi John \n Look into the following paths \n c:\\folder1\\folder2\n c:\\folder3\\folder4";
+            Console.WriteLine(text);
+
+            // we can use an verbatim string for the above case 
+            var text1 = @"Hi John
+                Look into the following paths 
+                c:\folder1\folder2 
+                c:\folder3\folder4";
+            Console.WriteLine(text1);
+        }
+    }
+}
