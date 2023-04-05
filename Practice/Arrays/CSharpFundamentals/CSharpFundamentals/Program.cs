fd38a486b4535b9d163f36e649275200c72b1886
Sahith Kumar <sahithkumar1999@gmail.com>
2023-04-05 08:12:33 +0200

    practicing the concept of arrays

---
+++ Practice/Arrays/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];// or var numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Sahith", "Kumar", "babu" };
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+        }
+    }
+}
