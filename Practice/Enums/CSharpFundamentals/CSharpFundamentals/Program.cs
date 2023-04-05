81dec9abe22c2734a1c6ecd5e78680ce82d0e670
Sahith Kumar <sahithkumar1999@gmail.com>
2023-04-05 19:33:09 +0200

    Enum practice

---
+++ Practice/Enums/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace CSharpFundamentals
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail =1,
+        RegisteredAirMail=2,
+        Express=3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            // converting to an string
+            Console.WriteLine(method.ToString());
+
+            // converting a string to enum
+
+            var methodName = "Express";
+            var ShippingMethod1=Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
