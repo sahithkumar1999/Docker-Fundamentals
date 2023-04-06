d5872bfdcfe95488322d6e0d92869b828a5e6924
Sahith Kumar <sahithkumar1999@gmail.com>
2023-04-06 13:48:27 +0200

    Exercises

---
+++ Exercise/Control flow exercise 1/Maximum/Maximum/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number1 = 10;
+            var number2 = 20;
+
+            if (number1 > number2)
+            {
+                Console.WriteLine("Number1 is maximum");
+            }
+            else if (number1 < number2)
+            {
+                Console.WriteLine("Number2 is maximum");
+            }
+        }
+    }
+}
---
+++ Exercise/Control flow exercise 1/NumberValidity/NumberValidity/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NumberValidity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Number = 9;
+
+            if (Number >= 1 && Number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+        }
+
+    }
+}
