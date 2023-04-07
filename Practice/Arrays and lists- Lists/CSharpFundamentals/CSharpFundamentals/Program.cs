33b126401d3c36d9d571958471511018f5cba908
Sahith Kumar <sahithkumar1999@gmail.com>
2023-04-07 10:48:05 +0200

    Practice of codes

---
+++ Practice/Arrays and lists- Lists/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpFundaments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1,2,3,4};
+            numbers.Add(1);// in Arrays we don't have add
+                           // method but in Lists we can do it
+
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Index of 1: "+ numbers.IndexOf(1));
+            Console.WriteLine("Last Index of 1: " + numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count:"+ numbers.Count);
+
+            numbers.Remove(1);
+
+            for (var i=0; i < numbers.Count; i++)
+                {
+                if (numbers[i] == 1)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+            }
+            
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            numbers.Clear();
+            Console.WriteLine("Count:" + numbers.Count);
+        }
+    }
+}
---
+++ Practice/File and FileInfo/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+
+            //Delete method
+            File.Delete(path);
+
+            if(File.Exists(path) )
+            {
+                //
+            }
+            var content = File.ReadAllBytes(path);
+
+            var fileInfo = new FileInfo(path);
+
+            fileInfo.CopyTo("...");
+
+            fileInfo.Delete();
+
+            if(fileInfo.Exists )
+            {
+                //....
+            }
+
+
+        }
+    }
+}
